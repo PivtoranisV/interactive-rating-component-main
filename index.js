303052e2c83bada0725b21ed1f2f2804bd60a375
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-08-30 17:15:14 +0400

    Added select rate function

--- index.js
+++ index.js
@@ -2,10 +2,19 @@
 const mainRating = document.querySelector(".rating");
 const thankYou = document.querySelector(".thank-you");
 const submit = document.querySelector(".btn_submit");
+const selectedRate = document.querySelector(".selected");
+const rates = document.querySelectorAll(".btn_number");
 
 // hide/unhide elements
 
 submit.addEventListener("click", () => {
   thankYou.classList.remove("hidden");
   mainRating.classList.add("hidden");
+});
+
+// select rate function
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    selectedRate.innerHTML = rate.innerHTML;
+  });
 });
